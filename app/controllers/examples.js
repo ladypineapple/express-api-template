ee6aa72aaaf787af186d775d8559176a6c4e8461
Antony Donovan <antony.donovan@generalassemb.ly>
2016-12-14 14:23:19 -0500

    Add before actions to find an example
    By id for show
    By id and currentUser for update/destroy

--- app/controllers/examples.js
+++ app/controllers/examples.js
@@ -1,10 +1,37 @@
 'use strict';
 
+const HttpError = require('lib/wiring/errors/http-error');
 const controller = require('lib/wiring/controller');
 const models = require('app/models');
 const Example = models.example;
 
 const authenticate = require('./concerns/authenticate');
+
+const setExample = (req, res, next) => {
+  Example.findById(req.params.id, (error, example) => {
+    error = error || !example && new HttpError(404);
+    if (error) {
+      return next(error);
+    }
+
+    req.example = example;
+    next();
+  });
+};
+
+const setExampleForCurrentUser = (req, res, next) => {
+  let modelId = req.params.id || null;
+  let ownerId = req.currentUser._id || null;
+  Example.findOne({ _id: modelId, _owner: ownerId }, (error, example) => {
+    error = error || !example && new HttpError(404);
+    if (error) {
+      return next(error);
+    }
+
+    req.example = example;
+    next();
+  });
+};
 
 const index = (req, res, next) => {
   Example.find()
@@ -12,10 +39,8 @@ const index = (req, res, next) => {
     .catch(err => next(err));
 };
 
-const show = (req, res, next) => {
-  Example.findById(req.params.id)
-    .then(example => example ? res.json({ example }) : next())
-    .catch(err => next(err));
+const show = (req, res) => {
+  res.json({ example: req.example });
 };
 
 const create = (req, res, next) => {
@@ -23,36 +48,20 @@ const create = (req, res, next) => {
     _owner: req.currentUser._id,
   });
   Example.create(example)
-    .then(example => res.json({ example }))
+    .then(example => res.status(201).json({ example }))
     .catch(err => next(err));
 };
 
 const update = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  Example.findOne(search)
-    .then(example => {
-      if (!example) {
-        return next();
-      }
-
-      delete req.body._owner;  // disallow owner reassignment.
-      return example.update(req.body.example)
-        .then(() => res.sendStatus(200));
-    })
+  delete req.body._owner;  // disallow owner reassignment.
+  req.example.update(req.body.example)
+    .then(() => res.sendStatus(204))
     .catch(err => next(err));
 };
 
 const destroy = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  Example.findOne(search)
-    .then(example => {
-      if (!example) {
-        return next();
-      }
-
-      return example.remove()
-        .then(() => res.sendStatus(200));
-    })
+  req.example.remove()
+    .then(() => res.sendStatus(204))
     .catch(err => next(err));
 };
 
@@ -64,4 +73,6 @@ module.exports = controller({
   destroy,
 }, { before: [
   { method: authenticate, except: ['index', 'show'] },
+  { method: setExample, only: ['show'] },
+  { method: setExampleForCurrentUser, only: ['update', 'destroy'] },
 ], });
