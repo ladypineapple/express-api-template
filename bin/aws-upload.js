50fb929c0c46d9c95ae65632231926db2fe24d06
ladypinetree <maureenpine@gmail.com>
2017-03-06 15:08:39 -0500

    Refactor bin script into a module
    Moved most of code from aws-upload.
    New file in lib directory for s3 upload piece.
    S3 upload modules can now be used throughout app.
    Aws-upload file now works by implementing lib s3 module.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -2,55 +2,13 @@
 
 require('dotenv').load();
 
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const fs = require('fs');
-
-const mime = require('mime');
-const path = require('path');
-const crypto = require('crypto');
+const s3Upload = require('../lib/s3-upload');
 
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
 
-let mimeType = mime.lookup(file.path);
-let ext = path.extname(file.path);
-let folder = (new Date()).toISOString().split('T')[0];
-let stream = fs.createReadStream(file.path);
-
-console.log('stream is ', stream);
-
-new Promise((resolve, reject) => {
-  crypto.randomBytes(16, (error, buffer)=> {
-    if (error) {
-      reject(error);
-    } else {
-      console.log('buffer is ', buffer);
-      console.log('buffer.toS is ', buffer.toString('hex'));
-      resolve(buffer.toString('hex'));
-    }
-  });
-})
-.then((filename)=> {
-  let params = {
-    ACL: 'public-read',
-    ContentType: mimeType,
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `${folder}/${filename}${ext}`,
-    Body: stream,
-  };
-
-  return new Promise((resolve, reject)=> {
-    s3.upload(params, function (error, data) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-})
-.then(console.log)
-.catch(console.log);
+s3Upload(file)
+  .then(console.log)
+  .catch(console.error);
