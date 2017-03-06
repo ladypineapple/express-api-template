bf13fda5144cc2116302ada3272b2d01e05535bb
ladypinetree <maureenpine@gmail.com>
2017-03-06 15:58:49 -0500

    Add Upload model and refactors bin script
    Adds Upload mongoose model requiring title and url as attributes.
    Implements this model in the bin/script so we test everything from
    command line -> AWS -> mongo which is all working correctly.
    Removed example model we don't need anymore.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -3,12 +3,24 @@
 require('dotenv').load();
 
 const s3Upload = require('../lib/s3-upload');
+const Upload = require('../app/models/upload');
+const mongoose = require('../app/middleware/mongoose');
 
+// Upload.create(title, url?);//
 let file = {
   path: process.argv[2],
   title: process.argv[3],
 };
 
 s3Upload(file)
+.then((s3Response) => {
+  //get the url//
+  let url = s3Response.Location;
+  return Upload.create({
+    title: file.title,
+    url: url,
+  });
+})
   .then(console.log)
-  .catch(console.error);
+  .catch(console.error)
+  .then(() => mongoose.connection.close());
